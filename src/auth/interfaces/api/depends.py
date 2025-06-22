7ca6107abd6080115433c93c7a96e5fc606857d4
GAKiknadze <gakiknadze@yandex.ru>
2025-06-22 04:54:29 +0300

    Refactor access token retrieval to raise InvalidCredentialsExc for empty tokens

--- src/auth/interfaces/api/depends.py
+++ src/auth/interfaces/api/depends.py
@@ -1,17 +1,25 @@
 from uuid import UUID
 
 from dishka.integrations.fastapi import FromDishka, inject
-from fastapi import Cookie, HTTPException, status
+from fastapi import Cookie, Depends
 
 from src.auth.application.dtos import AccessTokenRequestDTO
 from src.auth.application.use_cases import AuthorizeUseCase
+from src.auth.domain.exceptions import InvalidCredentialsExc
+
+
+async def get_access_token(access_token: str | None = Cookie(None)) -> str:
+    if access_token is None:
+        raise InvalidCredentialsExc("empty access token")
+
+    _, token = access_token.split(sep=" ", maxsplit=1)
+    return token
 
 
 @inject
 async def get_user_id(
-    use_case: FromDishka[AuthorizeUseCase], access_token: str | None = Cookie(None)
+    use_case: FromDishka[AuthorizeUseCase],
+    access_token: str = Depends(get_access_token),
 ) -> UUID:
-    if access_token is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     data = await use_case.execute(AccessTokenRequestDTO(access_token=access_token))
     return data.user_id
