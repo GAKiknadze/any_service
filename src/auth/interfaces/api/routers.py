84fd132e38489e2cc69dd4f8bc94514c9b08829e
GAKiknadze <gakiknadze@yandex.ru>
2025-06-22 04:55:22 +0300

    Refactor API routes to improve error handling and response structure

--- src/auth/interfaces/api/routers.py
+++ src/auth/interfaces/api/routers.py
@@ -1,7 +1,8 @@
 from uuid import UUID
 
 from dishka.integrations.fastapi import FromDishka, inject
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Cookie, Depends, status
+from fastapi.responses import JSONResponse
 
 from src.auth.application import dtos
 from src.auth.application.use_cases import (
@@ -12,87 +13,90 @@ from src.auth.application.use_cases import (
     RefreshTokenUseCase,
     RegisterUseCase,
 )
+from src.auth.domain.exceptions import InvalidCredentialsExc
 
-from .depends import get_user_id
+from .depends import get_access_token, get_user_id
+from .schemas import TokenResponse
+from .utils import create_new_tokens_response
 
 auth_router = APIRouter()
 
 
-@auth_router.post("/login", response_model=dtos.TokenResponseDTO)
+@auth_router.post(
+    "/login", response_model=TokenResponse, status_code=status.HTTP_200_OK
+)
 @inject
 async def login(
     request: dtos.LoginRequestDTO,
     use_case: FromDishka[LoginUseCase],
 ):
-    try:
-        return await use_case.execute(request)
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    tokens = await use_case.execute(request)
+
+    return create_new_tokens_response(tokens)
 
 
-@auth_router.post("/register", response_model=dtos.TokenResponseDTO)
+@auth_router.post(
+    "/register", response_model=TokenResponse, status_code=status.HTTP_201_CREATED
+)
 @inject
 async def register(
     request: dtos.RegisterRequestDTO,
     use_case: FromDishka[RegisterUseCase],
 ):
-    try:
-        return await use_case.execute(request)
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    tokens = await use_case.execute(request)
+
+    return create_new_tokens_response(tokens)
 
 
-@auth_router.post("/refresh", response_model=dtos.TokenResponseDTO)
+@auth_router.post(
+    "/refresh", response_model=TokenResponse, status_code=status.HTTP_200_OK
+)
 @inject
 async def refresh(
-    request: dtos.RefreshTokenRequestDTO,
     use_case: FromDishka[RefreshTokenUseCase],
+    refresh_token: str | None = Cookie(None),
 ):
-    try:
-        return await use_case.execute(request)
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    if refresh_token is None:
+        raise InvalidCredentialsExc("empty refresh token")
+
+    tokens = await use_case.execute(
+        dtos.RefreshTokenRequestDTO(refresh_token=refresh_token)
+    )
+
+    return create_new_tokens_response(tokens)
 
 
-@auth_router.post("/logout")
+@auth_router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
 @inject
 async def logout(
-    request: dtos.AccessTokenRequestDTO,
-    use_case: FromDishka[LogoutUseCase],
+    use_case: FromDishka[LogoutUseCase], access_token: str = Depends(get_access_token)
 ):
-    try:
-        await use_case.execute(request)
-        return {"detail": "Logged out"}
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    await use_case.execute(dtos.AccessTokenRequestDTO(access_token=access_token))
+    return JSONResponse(content={}, status_code=status.HTTP_204_NO_CONTENT)
 
 
 session_router = APIRouter()
 
 
-@session_router.get("/", response_model=dtos.SessionsResponseDTO)
+@session_router.get(
+    "/", response_model=dtos.SessionsResponseDTO, status_code=status.HTTP_200_OK
+)
 @inject
 async def get_sessions_list(
     use_case: FromDishka[GetSessionsUseCase],
     user_id: UUID = Depends(get_user_id),
 ):
-    try:
-        request = dtos.SessionsRequestDTO(user_id=user_id)
-        return await use_case.execute(request)
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    request = dtos.SessionsRequestDTO(user_id=user_id)
+    return await use_case.execute(request)
 
 
-@session_router.delete("/{session_id}")
+@session_router.delete("/{session_id}", status_code=status.HTTP_204_NO_CONTENT)
 @inject
 async def delete_session_by_id(
     session_id: UUID,
     use_case: FromDishka[DeleteSessionUseCase],
-    user_id: UUID = Depends(get_user_id),
+    _: UUID = Depends(get_user_id),
 ):
-    try:
-        request = dtos.DeleteSessionRequestDTO(session_id=session_id)
-        await use_case.execute(request)
-        return {"detail": "Session deleted"}
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+    request = dtos.DeleteSessionRequestDTO(session_id=session_id)
+    await use_case.execute(request)
+    return JSONResponse(content={}, status_code=status.HTTP_204_NO_CONTENT)
