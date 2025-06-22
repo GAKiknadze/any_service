cd4223be012c25c923265c0ca658c35776d2bb22
GAKiknadze <gakiknadze@yandex.ru>
2025-06-22 04:55:11 +0300

    Add TokenResponse schema and create_new_tokens_response utility function

---
+++ src/auth/interfaces/api/utils.py
@@ -0,0 +1,22 @@
+from fastapi.responses import JSONResponse
+
+from src.auth.application.dtos import TokenResponseDTO
+
+from .schemas import TokenResponse
+
+
+def create_new_tokens_response(tokens: TokenResponseDTO) -> JSONResponse:
+    response = JSONResponse(
+        content=TokenResponse(
+            access_token=tokens.access_token, access_token_type=tokens.token_type
+        )
+    )
+
+    response.set_cookie(
+        "refresh_token",
+        tokens.refresh_token,
+        expires=tokens.refresh_expires,
+        secure=True,
+    )
+
+    return response
